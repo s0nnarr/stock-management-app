b25f7845f095ed4eb2c85ce7a62f26263a0e36d0
2nio <antoniogeorgetoader@yahoo.ro>
2025-02-24 19:58:54 +0200

    structure update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
+import Inventory from './pages/inventory'
+import Orders from './pages/orders'
+import Company from './pages/company'
+import Login from './pages/auth/login'
+import Signup from './pages/auth/signup'
 
 function App() {
 
@@ -7,6 +12,11 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/inventory' element={<Inventory />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/company' element={<Company />} />
       </Routes>
     </>
   )
