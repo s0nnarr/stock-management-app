9834e9213ae7bcee63e5007b1e76c16d5c3ffa4d
ionut <opreaionut035@gmail.com>
2025-03-01 17:12:59 +0200

    Added addUser to company in Company controller/router + tested endpoint

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,5 @@
 const companyModel = require('../models/companyModel.js')
-
+const userModel = require('../models/userModel.js')
 
 const postCompany = async (req, res) => {
 
@@ -103,4 +103,35 @@ const getAuditLog = async (req, res) => {
 
 }
 
-module.exports = { getAuditLog, postCompany, getCompanyAbout, getCompanyUsers };
+const addUser = async (req, res) => {
+    try {
+       
+        const { companyID, userID, role } = req.body;
+        const company = await companyModel.findOne({ id: companyID });
+        if (!company) {
+            throw new Error('Company not found!');
+        }
+        const user = await userModel.findOne({ id: userID });
+        if (!user) {
+            throw new Error('User not found!');
+        }
+
+        const existingUser = company.users.find(u => u.user.toString() === user._id.toString() && u.role === role);
+        if (existingUser) {
+            throw new Error('User with the same role already exists!');
+        }
+
+        company.users.push({ user: user._id, role: role });
+        user.companies.push({ id: company._id, role: role });
+        await user.save();
+        await company.save();
+        res.status(200).json({ message: 'User added successfully!' });
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+
+
+module.exports = { getAuditLog, postCompany, getCompanyAbout, getCompanyUsers, addUser };
--- server/routes/companyRoute.js
+++ server/routes/companyRoute.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAuditLog,postCompany,getCompanyAbout,getCompanyUsers}=require('../controllers/companyController.js');
+const {getAuditLog,postCompany,getCompanyAbout,getCompanyUsers,addUser}=require('../controllers/companyController.js');
 
 router
     
     .get("/getauditlog",getAuditLog)
     .get("/getcompanyusers",getCompanyUsers)
-    .post("/",postCompany)
+    
     .get("/:id",getCompanyAbout)
+    .post("/",postCompany)
+    .post("/adduser",addUser)
     
     
 
