def860fb369ac627e0810d488bf30f2a9d96d9a2
Luca <s0narr@protonmail.com>
2025-03-03 18:45:44 +0200

    Added getOrdersByCompany route

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { createOrder, getAllOrders, deleteOrder } = require('../controllers/orderController');
+const { createOrder, getAllOrders, deleteOrder, getOrdersByCompany } = require('../controllers/orderController');
 
 router.post('/', createOrder);
 router.get('/', getAllOrders);
 router.delete('/:id', deleteOrder);
+router.get('/getordersbycompany', getOrdersByCompany);
 
 module.exports = router;
