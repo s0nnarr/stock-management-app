f52028c6bc21f9032b3484ea3173c103ce3c43e2
ionut <opreaionut035@gmail.com>
2025-03-04 19:03:41 +0200

    Added middlewares for company routes and tested all endpoints

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -5,32 +5,47 @@ const postCompany = async (req, res) => {
 
 
     try {
-        const { id, name, address, phone, currency } = req.body;
+        const { id, name, address, ownerID, phone, currency } = req.body;
+        if (!id || !name || !address || !phone || !currency) {
+            throw new Error('Please fill all fields!');
+        }
+        user = await userModel.findOne({
+            id:
+                ownerID
+        });
+        if (!user) {
+            throw new Error('User not found!');
+        }
+        const existingCompany = await companyModel.findOne
+            ({ id: id });
+        if (existingCompany) {
+            throw new Error('Company already exists!');
+        }
 
         const company = await companyModel.create({
             id,
             name,
+            owner: user._id,
             address,
             phone,
             currency
         });
-
+        user.companies.push({ id: company._id, role: 'owner' });
+        user.currentCompany = company._id;
+        company.users.push({ user: user._id, role: 'owner' });
+        await user.save();
+        await company.save();
         res.status(200).json({ message: 'Company created successfully' });
-
     }
     catch (error) {
         res.status(400).json({ message: error.message });
     }
-
-
 }
-
 
 const getCompanyAbout = async (req, res) => {
     try {
 
         const { id } = req.params;
-
         const company = await companyModel.findOne({ id: id }, { _id: 0, name: 1, address: 1, phone: 1, currency: 1 });
 
         if (!company) {
@@ -39,16 +54,11 @@ const getCompanyAbout = async (req, res) => {
         res.status(200).json({ company: company });
 
     }
-
-
     catch (error) {
         res.status(400).json({ message: error.message });
     }
 
-
 }
-
-
 
 const getCompanyUsers = async (req, res) => {
 
@@ -57,11 +67,10 @@ const getCompanyUsers = async (req, res) => {
         const page = req.query.page || 0;
         const size = req.query.size || 5;
         const company = await companyModel.findOne({ id: companyID }, { _id: 0, users: 1 }).skip(page * size).limit(size);
-
+        
         if (!company) {
             throw new Error('No users found!');
         }
-
         res.status(200).json({ users: company.users });
 
     }
@@ -73,39 +82,9 @@ const getCompanyUsers = async (req, res) => {
 
 
 
-
-
-
-const getAuditLog = async (req, res) => {
-
-    try {
-
-        const companyID = req.query.companyID;
-
-        const page = req.query.page || 0;
-        const size = req.query.size || 5;
-
-
-        const company = await companyModel.findOne({ id: companyID }, { _id: 0, auditLog: 1 }).skip(page * size).limit(size);
-
-        if (!company) {
-            throw new Error('No audit log found!');
-        }
-
-        res.status(200).json({ auditLog: company.auditLog });
-
-
-
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-
-}
-
 const addUser = async (req, res) => {
     try {
-       
+
         const { companyID, userID, role } = req.body;
         const company = await companyModel.findOne({ id: companyID });
         if (!company) {
@@ -134,4 +113,65 @@ const addUser = async (req, res) => {
 
 
 
-module.exports = { getAuditLog, postCompany, getCompanyAbout, getCompanyUsers, addUser };
+const removeUser = async (req, res) => {
+    try {
+        const { companyID, userID, role } = req.body;
+        const company = await companyModel.findOne({ id: companyID });
+        if(role==="owner"){
+            throw new Error('Owner cannot be removed!');
+        }
+        if (!company) {
+            throw new Error('Company not found!');
+        }
+        const user = await userModel.findOne({ id: userID });
+        if (!user) {
+            throw new Error('User not found!');
+        }
+
+        const userIndex = company.users.findIndex(u => u.user.toString() === user._id.toString() && u.role === role);
+        if (userIndex === -1) {
+            throw new Error('User not found in company!');
+        }
+
+        company.users.splice(userIndex, 1);
+        const companyIndex = user.companies.findIndex(c => c.id.toString() === company._id.toString() && c.role === role);
+        if (companyIndex !== -1) {
+            user.companies.splice(companyIndex, 1);
+        }
+
+        await user.save();
+        await company.save();
+        res.status(200).json({ message: 'User removed successfully!' });
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+const deleteCompany = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const company = await companyModel.findOneAndDelete({ id: id });
+
+        if (!company) {
+            throw new Error('Company not found!');
+        }
+
+        await userModel.updateMany(
+            { 'companies.id': company._id },
+            { $pull: { companies: { id: company._id } } }
+        );
+
+        await userModel.updateOne(
+            { _id: company.owner },
+            { $unset: { currentCompany: "" } }
+        );
+
+        res.status(200).json({ message: 'Company deleted successfully!' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+
+module.exports = { postCompany, getCompanyAbout, getCompanyUsers, addUser, removeUser, deleteCompany };
--- server/routes/companyRoute.js
+++ server/routes/companyRoute.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const verifyAccessToken = require('../middleware/verifyAccessToken')
+const verifyRole = require('../middleware/verifyRole')
 
-const {getAuditLog,postCompany,getCompanyAbout,getCompanyUsers,addUser}=require('../controllers/companyController.js');
+const {
+
+    postCompany,
+    getCompanyAbout,
+    getCompanyUsers,
+    addUser,
+    removeUser,
+    deleteCompany
+    }=require('../controllers/companyController.js');
+
+router.get("/getcompanyusers",verifyAccessToken,getCompanyUsers);
 
 router
+    .post("/",verifyAccessToken,postCompany)
+    .get("/:id",verifyAccessToken,getCompanyAbout)
+    .delete("/:id",verifyAccessToken, verifyRole(['owner', 'admin']),deleteCompany)
+   
+router.put("/adduser",verifyAccessToken, verifyRole(['owner', 'admin']),addUser);
+router.put("/removeuser",verifyAccessToken, verifyRole(['owner', 'admin']),removeUser);
     
-    .get("/getauditlog",getAuditLog)
-    .get("/getcompanyusers",getCompanyUsers)
-    
-    .get("/:id",getCompanyAbout)
-    .post("/",postCompany)
-    .post("/adduser",addUser)
     
     
 
