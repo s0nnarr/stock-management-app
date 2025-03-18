4014d8a7808b616953aa23d36d23c40446d8c175
2nio <antoniogeorgetoader@yahoo.ro>
2025-03-18 23:44:25 +0200

    dashboard frontend

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -4,19 +4,107 @@ import { AuthContext } from '../../context/authContext';
 import Sidebar from '../../components/sidebar';
 import Navbar from '../../components/navbar';
 import { useFetch } from '../../hooks/useFetch';
+import AreaReChart from '../../components/AreaReChart';
 
 function Dashboard() {
 
     const { user } = useContext(AuthContext)
 
     return (
-        <div>
+        <>
             <Sidebar tab={'Dashboard'} />
             <Navbar />
             <main className='dashboard-main'>
-                {user ? <h1>Welcome to {user.currentCompany.name}</h1> : <h1>No user...</h1>}
+                <div className='dashboard-div-stats'>
+                    <div className='dashboard-div-orders'>
+                        <h3 className='dashboard-h3-category'>Orders</h3>
+                        <ol>
+                            <li>
+                                <label>TO BE PROCESSED</label>
+                                <p>200</p>
+                            </li>
+                            <li>
+                                <label>TO BE SHIPPED</label>
+                                <p>360</p>
+                            </li>
+                            <li>
+                                <label>TO BE DELIVERED</label>
+                                <p>120</p>
+                            </li>
+                            <li>
+                                <label>TOTAL</label>
+                                <p>680</p>
+                            </li>
+                        </ol>
+                    </div>
+                    <div className='dashboard-div-orders'>
+                        <h3 className='dashboard-h3-category'>Inventory</h3>
+                        <div className='dashboard-div-inventory'>
+                            <div className='dashboard-div-inventoryStats'>
+                                <h4>High value inventory</h4>
+                                <ol>
+                                    <li>
+                                        <h3>Laptop IdeaPad i5</h3>
+                                        <p>58000€</p>
+                                    </li>
+                                    <li>
+                                        <h3>Kingston 16GB RAM</h3>
+                                        <p>18250€</p>
+                                    </li>
+                                    <li>
+                                        <h3>Macbook Pro M2</h3>
+                                        <p>18250€</p>
+                                    </li>
+                                </ol>
+                            </div>
+                            <div className='dashboard-div-inventoryStats'>
+                                <h4>Inventory status</h4>
+                                <ol>
+                                    <li>
+                                        <h3>Total</h3>
+                                        <p>426</p>
+                                    </li>
+                                    <li>
+                                        <h3>Low stock</h3>
+                                        <p>28</p>
+                                    </li>
+                                    <li>
+                                        <h3>Zero stock</h3>
+                                        <p>6</p>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='dashboard-div-orders'>
+                        <h3 className='dashboard-h3-category'>Sales</h3>
+                        <div className='dashboard-div-inventory'>
+                            <div className='dashboard-div-inventoryStats'>
+                                <h4>Revenue</h4>
+                                <AreaReChart />
+                            </div>
+                            <div className='dashboard-div-inventoryStats'>
+                                <h4>Top selling</h4>
+                                <ol>
+                                    <li>
+                                        <h3>Macbook Pro M2</h3>
+                                        <p>612</p>
+                                    </li>
+                                    <li>
+                                        <h3>Asus ROG Strix G15</h3>
+                                        <p>326</p>
+                                    </li>
+                                    <li>
+                                        <h3>RTX 5070 TI</h3>
+                                        <p>163</p>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main >
-        </div>
+        </>
     )
 }
 
